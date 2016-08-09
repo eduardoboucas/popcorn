f96c7f5f301d5b53ffa0c7796867a3c68d4b6d32
Eduardo Bouças <mail@eduardoboucas.com>
2016-08-09 17:25:43 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+(function ($) {
+  var $comments = $('.js-comments');
+
+  $('.js-form').submit(function () {
+    var form = this;
+
+    $(form).addClass('form--loading');
+
+    $.ajax({
+      type: $(this).attr('method'),
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      contentType: 'application/x-www-form-urlencoded',
+      success: function (data) {
+        showModal('Review submitted', 'Thanks for your review! It will show on the site once it has been approved.');
+        $(form).removeClass('form--loading');
+      },
+      error: function (err) {
+        console.log(err);
+        showModal('Error', 'Sorry, there was an error with the submission!');
+        $(form).removeClass('form--loading');
+      }
+    });
+
+    return false;
+  });
+
+  $('.js-close-modal').click(function () {
+    $('body').removeClass('show-modal');
+  });
+
+  function showModal(title, message) {
+    $('.js-modal-title').text(title);
+    $('.js-modal-text').html(message);
+
+    $('body').addClass('show-modal');
+  }
+})(jQuery);
